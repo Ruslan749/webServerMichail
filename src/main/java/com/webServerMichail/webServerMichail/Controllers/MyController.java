75a1fc0ba38001a4dae2c063ad2ca5c9855dbf94
ruslan749 <73393054+Ruslan749@users.noreply.github.com>
2023-05-28 20:02:36 +0500

    project ferst

--- src/main/java/com/webServerMichail/webServerMichail/Controllers/MyController.java
+++ src/main/java/com/webServerMichail/webServerMichail/Controllers/MyController.java
@@ -10,26 +10,25 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
-public class BlogController {
+public class MyController {
 
     @Autowired
     private PostRepository postRepository;
 
-    @GetMapping("/blog")
-    public String blogMain(Model model){
+    @GetMapping("/index")
+    public void formGet(Model model){
         Iterable<Post> posts = postRepository.findAll();
         model.addAttribute("posts", posts);
-        return "blog-main";
+    }
+    @GetMapping("/")
+    public String home(Model model){
+        return "index";
     }
 
-    @GetMapping("/blog/add")
-    public String blogAdd(Model model){
-        return "blog-add";
-    }
-    @PostMapping("/blog/add")
-    public String blogPostAdd (@RequestParam String title, @RequestParam String anons,@RequestParam String full_text, Model model){
-        Post post = new Post(title, anons,full_text);
+    @PostMapping("/")
+    public String formPostData (@RequestParam String name, @RequestParam String phone, Model model){
+        Post post = new Post(name, phone);
         postRepository.save(post);
-        return "redirect:/blog";
+        return "/index";
     }
 }
